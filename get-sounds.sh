f0171def5cb36bc956b44426031507285fbbfa28
jsirgo <me@jasirgo.com>
2019-06-22 16:33:10 +0200

    Updated get-sounds script

--- get-sounds.sh
+++ get-sounds.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Download sound files and sound data json from dmcallejo/lavidamoderna_bot project https://github.com/dmcallejo/lavidamoderna_bot
 git clone https://github.com/dmcallejo/lavidamoderna_bot.git
+mkdir -p resources/sounds
 cp -r  lavidamoderna_bot/LaVidaModerna/ resources/sounds/
+mkdir -p src/config
 cp lavidamoderna_bot/app/data.json src/config/data.json
 rm -rf lavidamoderna_bot
