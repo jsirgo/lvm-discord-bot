aeb3234a49c84d32b9b71992aed1e42e11a1ed7f
jsirgo <me@jasirgo.com>
2019-06-23 13:35:57 +0200

    Fixed error on get-sounds script

--- get-sounds.sh
+++ get-sounds.sh
@@ -2,7 +2,7 @@
 # Download sound files and sound data json from dmcallejo/lavidamoderna_bot project https://github.com/dmcallejo/lavidamoderna_bot
 git clone https://github.com/dmcallejo/lavidamoderna_bot.git
 mkdir -p resources/sounds
-cp -r  lavidamoderna_bot/LaVidaModerna/ resources/sounds/
+cp -r  lavidamoderna_bot/LaVidaModerna/* resources/sounds/
 mkdir -p src/config
 cp lavidamoderna_bot/app/data.json src/config/data.json
 rm -rf lavidamoderna_bot
